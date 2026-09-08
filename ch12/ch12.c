688b997c808328a019e70afb1d65c1d5701abd1c
Kanghohyeon-2600006 <hohyeongang428@gmail.com>
2026-09-08 20:37:57 +0900

    Update ch12.c

--- ch12/ch12.c
+++ ch12/ch12.c
@@ -1,1 +1,27 @@
-b
+// **********************************************
+// 제 목 : 포인터를 사용해 변수를 저장하고 출력
+// 날 짜 : 2026년 9월 8일
+// 작성자 : 2600006 강호현
+// **********************************************
+
+
+#define _CRT_SECURE_NO_WARNINGS 
+#pragma warning(disable:6031)  
+#include <stdio.h>
+
+int main(void)
+{
+    int a = -100;
+    char b = 'A';
+    double c = 3.14;
+
+    int* pa = &a;
+    char* pb = &b;
+    double* pc = &c;
+
+    printf("int형 변수 a의 값은 : %d\n", *pa);
+    printf("char형 변수 b의 값은 : %c\n", *pb);
+    printf("double형 변수 c의 값은 : %lf\n", *pc);
+
+    return 0;
+}
